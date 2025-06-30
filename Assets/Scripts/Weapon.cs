7ba886a81264c255f38db0f2207380315751da7a
Paras Chauhan <paraschauhan066@gmail.com>
2025-06-30 12:55:14 +0530

    Add muzzle flash prefab and integrate into weapon shooting mechanics
    - Created a new prefab for the muzzle flash effect.
    - Updated the Main scene to include the muzzle flash prefab under Pistol.
    - Modified the Weapon script to include a reference to the muzzle flash ParticleSystem and play it when shooting.
    - Adjusted post-processing settings in the Main scene for improved visual quality.
    - 0.9

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] int damageAmount = 1;
+    [SerializeField] ParticleSystem muzzleFlash;
+
     StarterAssetsInputs starterAssetsInputs;
 
 
@@ -20,7 +22,9 @@ public class Weapon : MonoBehaviour
     void HandleShoot()
     {
         if (!starterAssetsInputs.shoot) return;
-        
+
+        muzzleFlash.Play();
+
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
