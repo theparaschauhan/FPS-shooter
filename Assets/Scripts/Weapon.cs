401fe596b00adc3186bdb9c9594de9fe4c07a952
Paras Chauhan <paraschauhan066@gmail.com>
2025-07-01 12:08:46 +0530

    Added weapon animations and integrate shooting mechanics with animator. -0.10

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] int damageAmount = 1;
     [SerializeField] ParticleSystem muzzleFlash;
 
     StarterAssetsInputs starterAssetsInputs;
+
+    const string SHOOT_STRING = "Shoot";
 
 
     void Awake()
@@ -24,6 +27,8 @@ public class Weapon : MonoBehaviour
         if (!starterAssetsInputs.shoot) return;
 
         muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
 
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
@@ -31,8 +36,6 @@ public class Weapon : MonoBehaviour
             // Debug.Log(hit.collider.name);
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damageAmount);
-            
-            starterAssetsInputs.ShootInput(false);
         }  
     }
 }
