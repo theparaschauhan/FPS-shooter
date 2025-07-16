b2b5db5967bf9bfc89736fe83595520922fa7de1
Paras Chauhan <paraschauhan066@gmail.com>
2025-07-16 18:43:49 +0530

    Rename of WeaponOS to WeaponSO - SO - Scriptable Object

--- Assets/Scripts/WeaponOS.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "WeaponOS", menuName = "ScriptableObjects/WeaponSO")]
-public class WeaponOS : ScriptableObject
-{
-    public int Damage = 1;
-    public float FireRate = .5f;
-
-}
