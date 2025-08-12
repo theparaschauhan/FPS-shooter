85b77be734d6eedb571e927ab8f5c985f8e75850
Paras Chauhan <paraschauhan066@gmail.com>
2025-08-12 11:17:47 +0530

    Implement Win condition and Add Enemies left UI

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -5,18 +5,27 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] GameObject robotExplosionVFX;
     [SerializeField] int startingHealth = 3;
     int currentHealth;
+    GameManager gameManager;
 
     void Awake()
     {
         currentHealth = startingHealth;
     }
+    
+    void Start()
+    {
+        gameManager = FindFirstObjectByType<GameManager>();
+        gameManager.AdjustEnemiesLeft(1);
+    }
+
 
     public void TakeDamage(int amount)
     {
         currentHealth = currentHealth - amount;// current health - damage amount 
-        
+
         if (currentHealth <= 0)
         {
+            gameManager.AdjustEnemiesLeft(-1);
             SelfDestruct();
         }
     }
--- Assets/Scripts/Misc/GameManager.cs
+++ Assets/Scripts/Misc/GameManager.cs
@@ -1,8 +1,32 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text enemiesLeftText;
+    [SerializeField] GameObject youWinContainer;
+
+    int enemiesLeft = 0;
+
+    const string ENEMIES_LEFT_STRING = "Enemies Left: ";
+
+    public void AdjustEnemiesLeft(int amount)
+    {
+        enemiesLeft += amount;
+        enemiesLeftText.text = ENEMIES_LEFT_STRING + enemiesLeft.ToString();
+
+        if (enemiesLeft <= 0)
+        {
+            youWinContainer.SetActive(true);
+        }
+    }
+
+
+
+
+
+
     public void RestartLevelButton()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
